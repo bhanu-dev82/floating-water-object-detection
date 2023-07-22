77409680db2b8cf8f43b58b637ae2b934bc37654
Bhanu Nagpure <130910873+bhanu-dev82@users.noreply.github.com>
2023-07-23 00:49:07 +0530

    initial upload file for object detection
    file to handle the incoming images from esp32 for object detection

---
+++ didiserver.py
@@ -0,0 +1,58 @@
+import cv2
+import matplotlib.pyplot as plt
+import cvlib as cv
+import urllib.request
+import numpy as np
+from cvlib.object_detection import draw_bbox
+import concurrent.futures
+
+url = 'http://192.168.242.88/cam-mid.jpg'
+
+# Motor control function
+def control_motor(state):
+    # Send HTTP request to ESP32-CAM module to control motor
+    motor_url = 'http://192.168.242.88/motor?state=' + state
+    urllib.request.urlopen(motor_url)
+
+def run1():
+    cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
+    while True:
+        img_resp = urllib.request.urlopen(url)
+        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+        im = cv2.imdecode(imgnp, -1)
+
+        cv2.imshow('live transmission', im)
+        key = cv2.waitKey(5)
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+def run2():
+    cv2.namedWindow("detection", cv2.WINDOW_AUTOSIZE)
+    while True:
+        img_resp = urllib.request.urlopen(url)
+        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+        im = cv2.imdecode(imgnp, -1)
+
+        bbox, label, conf = cv.detect_common_objects(im)
+        im = draw_bbox(im, bbox, label, conf)
+
+        # Check if person is detected
+        if 'bottle' in label:
+            control_motor('on')
+        else:
+            control_motor('off')
+
+        cv2.imshow('detection', im)
+        key = cv2.waitKey(5)
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    print("started")
+    with concurrent.futures.ProcessPoolExecutor() as executer:
+        f1 = executer.submit(run1)
+        f2 = executer.submit(run2)
